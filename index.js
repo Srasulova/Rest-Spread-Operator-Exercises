d56eda1baac7bae4300e2efe7ee0ab4012e19f90
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-01-22 15:45:01 -0500

    finished exercise

--- index.js
+++ index.js
@@ -28,3 +28,57 @@ const doubleAndReturnArgs = (arr, ...args) => {
   return arr;
 };
 console.log(doubleAndReturnArgs([1, 2, 3], 4, 4));
+
+/** remove a random element in the items array
+and return a new array without that item. */
+
+// function removeRandom(items) {
+//   const randomIndex = Math.floor(Math.random() * items.length);
+//   items.splice(randomIndex, 1);
+//   return items;
+// }
+
+// const removeRandom = items => {
+//     let idx = Math.floor(Math.random() * items.length);
+//     return [...items.slice(0, idx), ...items.slice(idx + 1)];
+//   }
+
+removeRandom([1, 2, 3, 4]);
+
+console.log([1, 2, 3, 5, 7, 9, 10]);
+console.log(removeRandom([1, 2, 3, 5, 7, 9, 10]));
+
+/** Return a new array with every item in array1 and array2. */
+
+function extend(array1, array2) {
+  return [...array1, array2];
+}
+
+/** Return a new object with all the keys and values
+from obj and a new key/value pair */
+
+function addKeyVal(obj, key, val) {
+  return { ...obj, [key]: val };
+}
+
+/** Return a new object with a key removed. */
+
+function removeKey(obj, key) {
+  let newObj = { ...obj };
+  delete newObj[key];
+  return newObj;
+}
+
+/** Combine two objects and return a new object. */
+
+function combine(obj1, obj2) {
+  return { ...obj1, ...obj2 };
+}
+
+/** Return a new object with a modified key and value. */
+
+function update(obj, key, val) {
+  let newObj = { ...obj };
+  newObj[key] = val;
+  return newObj;
+}
